f79204f6de12c8b98bb04af6934e847e0c3019b3
wicky1510 <r.admiraal@gen25.com>
2020-06-08 22:11:27 +0200

    buy now

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+
+function toggleSizes() {
+    document.body.classList.toggle('show-sizes')
+}
+
+function enableAddToCart() {
+    document.getElementById('add-to-cart').classList.remove('disabled')
+}
